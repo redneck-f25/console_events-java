6202dbb433c991e219c66602423522c1dc2a12ea
Daniel Hammerschmidt <daniel@rendenck-engineering.com>
2017-07-23 17:16:45 +0200

    improve CTRL_C_EVENT

--- src/console_events/ConsoleEvents.java
+++ src/console_events/ConsoleEvents.java
@@ -2,13 +2,16 @@ package console_events;
 
 
 import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.User32;
 import com.sun.jna.platform.win32.WinBase.PROCESS_INFORMATION;
 import com.sun.jna.platform.win32.WinBase.STARTUPINFO;
 import com.sun.jna.platform.win32.WinDef.DWORD;
+import com.sun.jna.platform.win32.WinDef.WORD;
+import com.sun.jna.platform.win32.WinNT.HANDLE;
 
 public class ConsoleEvents {
 	
-	static String CMDLINE_BASE = String.format(
+	static String JAVA_CMDLINE_BASE = String.format(
 			"\"%s%s\" -Dfile.encoding=%s -classpath \"%s\" %s",
 			System.getProperty("java.home"),
 			"\\bin\\java.exe",
@@ -27,39 +30,53 @@ public class ConsoleEvents {
 	}
 	
 	// wrap around CreateProcess()
-	static int create_process(String cmdline, int flags) {
+	static PROCESS_INFORMATION create_process(String cmdline, int flags, boolean inherit_std_handles) {
 		STARTUPINFO si = new STARTUPINFO();
 		PROCESS_INFORMATION pi = new PROCESS_INFORMATION();
 		
+		if (inherit_std_handles) {
+			si.dwFlags = Kernel32.STARTF_USESTDHANDLES | Kernel32.STARTF_USESHOWWINDOW;
+			si.hStdInput = Kernel32.INSTANCE.GetStdHandle(Kernel32.STD_INPUT_HANDLE);
+			si.hStdOutput = Kernel32.INSTANCE.GetStdHandle(Kernel32.STD_OUTPUT_HANDLE);
+			si.hStdError = Kernel32.INSTANCE.GetStdHandle(Kernel32.STD_ERROR_HANDLE);
+			si.wShowWindow = new WORD(User32.SW_HIDE);
+		}
+		
 		Kernel32.INSTANCE.CreateProcess(
-				null, cmdline, null, null, false,
+				null, cmdline, null, null, inherit_std_handles,
 				new DWORD(flags), null, null, si, pi);
-		//close handles immediately to avoid resource leaks
-		Kernel32.INSTANCE.CloseHandle(pi.hProcess);
-		Kernel32.INSTANCE.CloseHandle(pi.hThread);
-		return pi.dwProcessId.intValue();
+		return pi;
 	}
 	
-	static int create_child(String cmdline) {
-		int child_pid = create_process(cmdline, Kernel32.CREATE_NEW_CONSOLE);
-		System.out.println("Child process started: " + child_pid);
+	static PROCESS_INFORMATION create_child(String cmdline, boolean inherit_std_handles) {
+		PROCESS_INFORMATION pi = create_process(cmdline, Kernel32.CREATE_NEW_CONSOLE, inherit_std_handles);
+		System.out.println("+   Child process started: " + pi.dwProcessId);
 		sleep(1000);
-		return child_pid;
+		return pi;
 	}
 	
-	static void terminate_child(int child_pid) {
+	static void terminate_child(PROCESS_INFORMATION pi) {
+		int pid = pi.dwProcessId.intValue();
 		create_process(
-				String.format("%s %s %d", CMDLINE_BASE, "send_ctrl_c", child_pid),
-				Kernel32.DETACHED_PROCESS);
-		System.out.println("Child process terminated: " + child_pid);
-		sleep(1500);
+				String.format("%s %s %d", JAVA_CMDLINE_BASE, "send_ctrl_c", pid),
+				Kernel32.DETACHED_PROCESS,
+				true);
+		Kernel32.INSTANCE.WaitForSingleObject((HANDLE) pi.hProcess, Kernel32.INFINITE);
+		System.out.println("+   Child process terminated: " + pid);
+		// close handles to avoid resource leaks (zombie processes)
+		Kernel32.INSTANCE.CloseHandle((HANDLE) pi.hProcess);
+		Kernel32.INSTANCE.CloseHandle((HANDLE) pi.hThread);
 	}
 	
 	static void main_child(String[] args) {
-		Kernel32.INSTANCE.SetConsoleTitle("Hello! I am " + Kernel32.INSTANCE.GetCurrentProcessId() + ".");
+		String message = "++  Hello! I am " + Kernel32.INSTANCE.GetCurrentProcessId() + ".";
+		Kernel32.INSTANCE.SetConsoleTitle(message);
+		System.out.println(message);
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 			public void run() {
-				Kernel32.INSTANCE.SetConsoleTitle("Bye Bye " + Kernel32.INSTANCE.GetCurrentProcessId() + ".");
+				String message = "++  Bye Bye " + Kernel32.INSTANCE.GetCurrentProcessId() + ".";
+				Kernel32.INSTANCE.SetConsoleTitle(message);
+				System.out.println(message);
 				ConsoleEvents.sleep(1000);
 			}
 		});
@@ -68,6 +85,7 @@ public class ConsoleEvents {
 	
 	static void main_send_ctrl_c(String[] args) {
 		// needs to be created with DETACHED_PROCESS
+		System.out.println(String.format("%s %s", "+++ Send CTRL_C_EVENT to", args[1]));
 		Kernel32.INSTANCE.AttachConsole(Integer.parseInt(args[1]));
 		Kernel32.INSTANCE.GenerateConsoleCtrlEvent(Kernel32.CTRL_C_EVENT, 0);
 	}
@@ -88,29 +106,38 @@ public class ConsoleEvents {
 			return;
 		}
 		
+		String message = "+   Hello! I am " + Kernel32.INSTANCE.GetCurrentProcessId() + ".";
+		Kernel32.INSTANCE.SetConsoleTitle(message);
+		System.out.println(message);
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				String message = "+   Bye Bye " + Kernel32.INSTANCE.GetCurrentProcessId() + ".";
+				Kernel32.INSTANCE.SetConsoleTitle(message);
+				System.out.println(message);
+				ConsoleEvents.sleep(2000);
+			}
+		});
+		
 		// start child processes
 		
-		int child_pids[] = new int[10];
+		PROCESS_INFORMATION children[] = new PROCESS_INFORMATION[10];
 		int ii = 0;
 		
-		child_pids[ii++] = create_child("cmd.exe /c test.cmd");
-		child_pids[ii++] = create_child("ping -t 127.0.0.1");
+		children[ii++] = create_child("cmd.exe /c test.cmd", false);
+		children[ii++] = create_child("ping -t 127.0.0.1", true);
 		
 		for (int ik = 0; ik < 3; ++ik) {
-			child_pids[ii++] = create_child(String.format("%s %s", CMDLINE_BASE, "child"));
+			children[ii++] = create_child(String.format("%s %s", JAVA_CMDLINE_BASE, "child"), true);
 		}
 		
-		child_pids[ii++] = create_child("cmd.exe /c \"echo Be patient! & timeout /t -1 /nobreak\"");
+		children[ii++] = create_child("cmd.exe /c \"echo Be patient! & timeout /t -1 /nobreak\"", false);
 		
 		sleep(2500);
 		
 		// send signals to gracefully shutdown
 		
 		for (; ii-- > 0; ) {
-			terminate_child(child_pids[ii]);
+			terminate_child(children[ii]);
 		}
-		
-		sleep(4000);
 	}
-
 }
